4113e7d4d2abbe901f5695a219253dfa18dcf067
victorsalazart <68044892+victorsalazart@users.noreply.github.com>
2020-07-14 10:06:51 -0500

    Add files via upload

---
+++ Ejercicio 2 triangulo 1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Valor de b : ");
+            double b2 = double.Parse(Console.ReadLine());
+            double b2Rad = b2 * Math.PI / 180;
+
+            Console.Write(" Valor de z: ");
+            double z2 = double.Parse(Console.ReadLine());
+
+            Console.Write(" Valor de y: ");
+            double y2 = double.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
---
+++ ejercicio 1 porgramacion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        // Entrada del usuario de la variable X1
+        Console.Write("Ingrese X1: ");
+        double x1 = double.Parse(Console.ReadLine());
+
+        // Entrada del usuario de la variable Y1
+        Console.Write("Ingrese Y1: ");
+        double y1 = double.Parse(Console.ReadLine());
+
+        // Entrada del usuario de las variables restantes
+        Console.Write("Ingrese X2: ");
+        double x2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese Y2: ");
+        double y2 = double.Parse(Console.ReadLine());
+
+        double m = (y2 - y1) / (x2 - x1);
+
+        double b = y1 - (m * x1);
+
+        double d = (Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+        Console.WriteLine("La pendiente es: " + m);
+        Console.WriteLine("El intercepto es: " + b);
+        Console.WriteLine("La distancia es: " + d);
+
+    }
+}
